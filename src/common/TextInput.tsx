26bd94be929cfe46c50ad6a69cd9890a98d94efe
Danbi Choi <sweetrain05@hotmail.com>
2023-11-21 21:46:39 -0800

    contact form updated

---
+++ src/common/TextInput.tsx
@@ -0,0 +1,104 @@
+import { useMemo } from 'react';
+import classNames from 'classnames';
+import './TextInput.scss';
+
+export enum TextInputType {
+    TEXT = 'text',
+    TEXTAREA = 'textarea',
+    EMAIL = 'email',
+    PASSWORD = 'password',
+}
+
+type TextInputProps = {
+    type?: TextInputType;
+    icon?: React.ComponentType<React.SVGAttributes<SVGElement>>;
+    label?: string;
+    className?: string;
+    disabled?: boolean;
+    isIncludingErrorText?: boolean;
+    errorText?: string;
+    [key: string]: any;
+};
+
+const TextInput: React.FC<TextInputProps> = ({
+    type = TextInputType.TEXT,
+    icon: Icon,
+    label,
+    className = '',
+    disabled = false,
+    isIncludingErrorText = false,
+    errorText,
+    ...rest
+}) => {
+    const inputClasses = useMemo(
+        () =>
+            classNames(
+                'text-input',
+                {
+                    'text-input--include-error': isIncludingErrorText,
+                },
+                className
+            ),
+        [isIncludingErrorText, className]
+    );
+
+    const inputIconClasses = useMemo(
+        () =>
+            classNames(
+                'text-input__icon',
+                { 'text-input__icon--error': errorText },
+                {
+                    'text-input__icon--disabled': disabled,
+                }
+            ),
+        [errorText, disabled]
+    );
+
+    const inputFieldClasses = useMemo(
+        () =>
+            classNames(
+                'text-input__field',
+
+                { 'text-input__field--error': errorText },
+                {
+                    'text-input__field--disabled': disabled,
+                },
+
+                { 'text-input__field--no-icon': !Icon }
+            ),
+        [errorText, disabled, Icon]
+    );
+
+    return (
+        <div className={inputClasses}>
+            {label && Icon && (
+                <div className='text-input__label'>
+                    <Icon />
+                    <label htmlFor={label}>{label}</label>
+                </div>
+            )}
+
+            {type === TextInputType.TEXTAREA ? (
+                <textarea
+                    className={inputFieldClasses}
+                    disabled={disabled}
+                    {...rest}
+                />
+            ) : (
+                <input
+                    id={label}
+                    className={inputFieldClasses}
+                    type={type}
+                    disabled={disabled}
+                    {...rest}
+                />
+            )}
+
+            {errorText && (
+                <span className='text-input__error-text'>{errorText}</span>
+            )}
+        </div>
+    );
+};
+
+export default TextInput;
--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,15 +1,25 @@
+import {
+    BsEnvelopeFill,
+    BsFillChatLeftTextFill,
+    BsPencilFill,
+    BsPersonFill,
+} from 'react-icons/bs';
 import { contactLinks } from '../../common/AppConstants';
-
+import TextInput, { TextInputType } from '../../common/TextInput';
 import './Contact.scss';
 
 const Contact: React.FC = () => {
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit');
+    };
     return (
         <>
             <section className='contact'>
                 <div className='contact__img-container'>
                     <img
                         className='contact__img-container__img'
-                        src='https://images.unsplash.com/photo-1603477849227-705c424d1d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dHJvcGljYWwlMjBiZWFjaHxlbnwwfHwwfHx8MA%3D%3D'
+                        src='https://catastic.pet/wp-content/uploads/2023/04/cute-white-british-cat-wearing-sunglasses-yellow-fabric-hammock-isolated-yellow-background-1.jpg'
                         alt='profile'
                     />
                 </div>
@@ -31,6 +41,46 @@ const Contact: React.FC = () => {
                         </li>
                     ))}
                 </ul>
+                <div className='contact__form'>
+                    <h1 className='contact__form__title section-title'>
+                        Contact
+                    </h1>
+                    <p className='contact__form__subtitle'>
+                        Want to chat about my works?
+                    </p>
+                    <form
+                        onSubmit={handleFormSubmit}
+                        className='contact__form__input-container'
+                    >
+                        <TextInput
+                            type={TextInputType.TEXT}
+                            label='Full Name'
+                            icon={BsPersonFill}
+                            placeholder='Your name'
+                        />
+                        <TextInput
+                            type={TextInputType.EMAIL}
+                            label='Email'
+                            icon={BsEnvelopeFill}
+                            placeholder='Your email'
+                        />
+                        <TextInput
+                            type={TextInputType.TEXT}
+                            label='Subject'
+                            icon={BsPencilFill}
+                            placeholder='What is this about?'
+                        />
+                        <TextInput
+                            type={TextInputType.TEXTAREA}
+                            label='Message'
+                            icon={BsFillChatLeftTextFill}
+                            placeholder='Your message here...'
+                        />
+                        <button className='contact__form__input-container__btn btn'>
+                            Send
+                        </button>
+                    </form>
+                </div>
             </section>
         </>
     );
